cc8be093617dc5adb3b694376ef46eb1e85d0a62
Krishang Sharma <krishang.sharma.17704@gmail.com>
2025-01-01 23:01:43 +0530

    New branch for NextJS+Setup

---
+++ app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { Inter, Montserrat } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+});
+
+const montserrat = Montserrat({
+  variable: "--font-montserrat",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Krishang Sharma | Portfolio",
+  description:
+    "I am a 20 years old Self Taught Full Stack Developer, based in Delhi, India. I am currently pursuing BCA from IP University. Visit my portoflio to know more.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.variable} ${montserrat.variable} antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
+}
